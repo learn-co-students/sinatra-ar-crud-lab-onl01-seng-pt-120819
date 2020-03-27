02ccf3dfae2512f0a5461c528b7c1307d8412ab9
asif-kamal <asif.kamal1489@gmail.com>
2020-03-27 19:44:51 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
 
   get '/' do
@@ -16,38 +22,35 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-    Post.create(params)
-    redirect '/articles'
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
   end
 
-  get '/articles' do
-    @articles = Article.all
-    erb :index
-  end
 
   get '/articles/:id' do
-    @articles = Article.find(params["id"])
+
+    @article = Article.find(params[:id])
     erb :show
   end
 
   get '/articles/:id/edit' do
-    @articles = Article.find(params["id"])
+    @article = Article.find(params["id"])
     erb :edit
   end
 
   patch '/articles/:id' do
-    id = params["id"]
+
     new_params = {}
-    old_post = Article.find(id)
-    new_params[:name] = params["name"]
-    new_params[:content] = params["content"]
+    old_post = Article.find(params[:id])
+    new_params[:title] = params[:title]
+    new_params[:content] = params[:content]
     old_post.update(new_params)
 
     redirect "/articles/#{id}"
   end
 
-  delete '/articles/:id/delete' do
-    @article = Article.find(params["id"])
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
     @article.destroy
     erb :delete
   end
